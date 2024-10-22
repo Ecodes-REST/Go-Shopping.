3b851fb75309fa1f55729f3ce4715bf190d3ce7b
Ecodes <shammahcodes@gmail.com>
2024-10-22 16:05:41 +0300

    shop..with..us

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -38,5 +38,5 @@ EMAIL_PORT = os.environ.get('EMAIL_PORT', 2525)
 #HTTPS settings
 SESSION_COOKIE_SECURE= True
 CSRF_COOKIE_SECURE= True
-CSRF_TRUSTED_ORIGINS = ['go-shopping-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://go-shopping-production.up.railway.app']
 
